e84a0f85466deb60440db66693b2a801b698efd5
alfnpnd <alfnpnd@gmail.com>
2023-09-05 14:33:45 +0700

    add navbar and hero section

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//toggle active
+
+let navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//KLIK DI LUAR SIDEBAR
+let hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
