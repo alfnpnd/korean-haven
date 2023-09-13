d7b6ca1763ceccecf41fd20d0d2df0d3b694a378
alfnpnd <alfnpnd@gmail.com>
2023-09-13 11:37:52 +0700

    add search

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 //toggle active
 
 let navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
 //KLIK DI LUAR SIDEBAR
@@ -10,5 +11,23 @@ let hamburger = document.querySelector("#hamburger-menu");
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+
+//toggle class active untuk search form
+let searchForm = document.querySelector(".search-form");
+let searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//klik diluar search
+let sb = document.querySelector("#search-button");
+document.addEventListener("click", function (e) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
